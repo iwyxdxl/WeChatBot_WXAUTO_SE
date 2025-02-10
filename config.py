f285c93e42a7b58e15b9ead10f073165047efa20
iwyxdxl <iwyxdxl@gmail.com>
2025-02-10 21:33:34 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+# 用户列表(请配置要和bot说话的账号的昵称，不要写备注！)
+# 例如：LISTEN_LIST = ['用户1','用户2']
+LISTEN_LIST = ['用户1']
+# DeepSeek API 配置
+DEEPSEEK_API_KEY = 'sk-'
+# 硅基流动API注册地址，免费15元额度 https://cloud.siliconflow.cn/
+DEEPSEEK_BASE_URL = 'https://api.siliconflow.cn/v1/'
+# 如果要使用官方的API
+# DEEPSEEK_BASE_URL = 'https://api.deepseek.com'
+# 硅基流动API的模型
+MODEL = 'Pro/deepseek-ai/DeepSeek-V3'
+# 回复最大token
+MAX_TOKEN = 2000
+#温度
+TEMPERATURE = 1.3
+# Moonshot AI配置（用于图片和表情包识别）
+# API申请https://platform.moonshot.cn/
+MOONSHOT_API_KEY = 'sk-'
+MOONSHOT_BASE_URL = "https://api.moonshot.cn/v1"
+MOONSHOT_TEMPERATURE = 0.3
+#表情包存放目录
+EMOJI_DIR = 'emojis'
